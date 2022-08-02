625db603d731f442f5f027076844c3acf7bb44b4
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-08-02 11:16:36 +0300

    hash route instance base url - for netlify

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const _instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
     withCredentials: true,
 })
 
@@ -9,3 +9,6 @@ export const _instance = axios.create({
 //|| 'http://localhost:7542/2.0/'
 
 //'https://neko-back.herokuapp.com/2.0/'
+
+
+///process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/'
