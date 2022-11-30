a5ca9bb459814259a98db3de8b77f728842e2f4a
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-11-30 03:09:00 +0300

    before full refactoring

---
+++ src/pages/Forms/Login/loginReducer.ts
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+const slice = createSlice({
+   name: 'login',
+   initialState: '',
+   reducers: {
+
+   }
+})
+export const loginReducer = slice.reducer
--- src/pages/PacksPage/Packs/PacksSearch/PacksSearch.tsx
+++ src/pages/PacksPage/Packs/PacksSearch/PacksSearch.tsx
@@ -1,40 +1,45 @@
-import { IconButton, TextField } from "@mui/material"
-import React, { ChangeEvent, useEffect, useState } from "react"
-import ClearIcon from '@mui/icons-material/Clear';
-export const PacksSearch: React.FC<PropsType> = React.memo((
-   { onSearchPacks }
-) => {
-   const [value, setValue] = useState('')
-   const [isSearching, setIsSearching] = useState(false)
-   useEffect(() => {
-      if (!isSearching) { return }
+import { IconButton, TextField } from "@mui/material";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import ClearIcon from "@mui/icons-material/Clear";
+export const PacksSearch: React.FC<PropsType> = React.memo(
+  ({ onSearchPacks }) => {
+    const [value, setValue] = useState("");
+    const [isSearching, setIsSearching] = useState(false);
+    useEffect(() => {
+      if (!isSearching) {
+        return;
+      }
       const id = setTimeout(() => {
-         onSearchPacks(value)
-         setIsSearching(false)
-      }, 1000)
-      return () => clearTimeout(id)
-   }, [value, isSearching, onSearchPacks])
-   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-      setValue(e.currentTarget.value)
-      setIsSearching(true)
-   }
-   const clearSearchValueHandler = () => {
-      setIsSearching(false)
-      setValue('')
-      onSearchPacks('')
-   }
-   return (
+        onSearchPacks(value);
+        setIsSearching(false);
+      }, 1000);
+      return () => clearTimeout(id);
+    }, [value, isSearching, onSearchPacks]);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setValue(e.currentTarget.value);
+      setIsSearching(true);
+    };
+    const clearSearchValueHandler = () => {
+      setIsSearching(false);
+      setValue("");
+      onSearchPacks("");
+    };
+    return (
       <div className="packs__search">
-         <TextField
-            id="standard-basic"
-            value={value}
-            label="Search..."
-            variant="standard"
-            onChange={onChangeHandler} />
-         <IconButton onClick={clearSearchValueHandler}><ClearIcon fontSize="large" /></IconButton>
+        <TextField
+          id="standard-basic"
+          value={value}
+          label="Search..."
+          variant="standard"
+          onChange={onChangeHandler}
+        />
+        <IconButton onClick={clearSearchValueHandler}>
+          <ClearIcon fontSize="large" />
+        </IconButton>
       </div>
-   )
-})
+    );
+  }
+);
 type PropsType = {
-   onSearchPacks: (value: string) => void
-}
+  onSearchPacks: (value: string) => void;
+};
