32796fc6595d1c601e609ad645b975e89c85fe01
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-12-02 06:08:43 +0300

    feat: new password

--- src/features/auth/new-password/NewPassword.tsx
+++ src/features/auth/new-password/NewPassword.tsx
@@ -1,80 +1,39 @@
-import { ChangeEvent, useState } from 'react';
-
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { CircularProgress, TextField } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 
-import style from './new-password.module.scss';
+import { NewPasswordForm } from './new-password-form';
 
 import { selectAuthNotice, selectAuthStatus } from 'common/selectors';
 import { CustomizedSnackbar } from 'components/customized-snackbar';
-import { FormFooter, FormTitle } from 'components/form-components';
+import { ParticlesContainer } from 'components/particles-container';
 import { useAppDispatch } from 'store/hooks';
-import { setNewPassword, setNotice } from 'store/reducers/auth-reducer';
+import { setNotice } from 'store/reducers/auth-reducer';
+import './newPassword.scss';
 
 export const NewPassword = (): JSX.Element => {
   const dispatch = useAppDispatch();
-  const params = useParams();
-
-  const [password, setPassword] = useState<string>('');
-  const [visibility, setVisibility] = useState<boolean>(false);
-
-  const token = params.token as string;
 
   const status = useSelector(selectAuthStatus);
 
   const notice = useSelector(selectAuthNotice);
 
-  const onClickHandler = (): void => {
-    dispatch(setNewPassword({ password, resetPasswordToken: token }));
-  };
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>): void => {
-    setPassword(e.currentTarget.value);
-  };
   const onCloseSnackbar = (): void => {
     dispatch(setNotice({ notice: '' }));
   };
-  const setVisibilityHandler = (): void => {
-    setVisibility(!visibility);
-  };
 
   return (
-    <>
-      <div className={style.container}>
-        {status === 'pending' && (
-          <CircularProgress style={{ zIndex: '3', position: 'absolute' }} />
-        )}
-        <div className={style.block}>
-          <FormTitle title="Create new password" />
-          <div className={style.inputBlock}>
-            <TextField
-              onChange={onChangeHandler}
-              className={style.input}
-              value={password}
-              type={visibility ? 'text' : 'password'}
-              label="Password"
-              variant="standard"
-            />
-            <span
-              role="presentation"
-              className={style.visibilityIcon}
-              onClick={setVisibilityHandler}
-            >
-              {visibility ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
-            </span>
-          </div>
-          <p>Create new password and we will send you further instructions to email</p>
-          <FormFooter onClick={onClickHandler} buttonTitle="Create new password" />
-        </div>
-      </div>
+    <div className="new-password-page">
+      <ParticlesContainer />
+      {status === 'pending' && (
+        <CircularProgress style={{ zIndex: '3', position: 'absolute' }} />
+      )}
+      <NewPasswordForm />
       <CustomizedSnackbar
         message={notice}
         isOpen={!!notice}
         onClose={onCloseSnackbar}
         isError={status === 'failed'}
       />
-    </>
+    </div>
   );
 };
--- src/features/auth/new-password/new-password-form/newPasswordForm.tsx
+++ src/features/auth/new-password/new-password-form/newPasswordForm.tsx
@@ -1,3 +1,58 @@
-export const newPasswordForm = (): JSX.Element => {
-  return <div>newPasswordForm</div>;
+import { useFormik } from 'formik';
+import { useParams } from 'react-router-dom';
+
+import style from './newPasswordForm.module.scss';
+
+import { validationForm } from 'common/utils';
+import { FormFooter, FormPassword } from 'components/form-components';
+import { useAppDispatch } from 'store/hooks';
+import { setNewPassword } from 'store/reducers/auth-reducer';
+
+export const NewPasswordForm = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  const params = useParams();
+  const token = params.token as string;
+
+  const handlePasswordFocus = (): void => {
+    formik.setTouched({
+      password: false,
+    });
+  };
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+    },
+    validate: values => {
+      const errors = validationForm(values);
+
+      if (errors.password) {
+        return errors;
+      }
+    },
+    onSubmit: async values => {
+      const action = await dispatch(
+        setNewPassword({ password: values.password, resetPasswordToken: token }),
+      );
+
+      if (setNewPassword.fulfilled.match(action)) {
+        console.log('true');
+      }
+    },
+  });
+
+  return (
+    <div className={style.new_password_form}>
+      <FormPassword
+        isError={formik.touched.password && !!formik.errors.password}
+        errorText={formik.errors.password}
+        onFocus={handlePasswordFocus}
+        fieldProps={formik.getFieldProps('password')}
+      />
+      <p className={style.text}>
+        Create new password and we will send you further instructions to email
+      </p>
+      <FormFooter onClick={formik.submitForm} buttonTitle="Create new password" />
+    </div>
+  );
 };
